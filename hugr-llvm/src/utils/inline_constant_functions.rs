202248039d083d26891b26b5dea9699bec8463af
Alan Lawrence <alan.lawrence@cambridgequantum.com>
2025-12-19 15:03:33 +0000

    Deprecate (unused) inline_constant_functions

--- hugr-llvm/src/utils/inline_constant_functions.rs
+++ hugr-llvm/src/utils/inline_constant_functions.rs
@@ -12,6 +12,7 @@ fn const_fn_name(konst_n: Node) -> String {
     format!("const_fun_{}", konst_n.index())
 }
 
+#[deprecated(note="Will be removed along with Value::Function")]
 pub fn inline_constant_functions(hugr: &mut impl HugrMut<Node = Node>) -> Result<()> {
     while inline_constant_functions_impl(hugr)? {}
     Ok(())
