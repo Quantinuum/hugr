a48d521b0260cb48c27443bffa41e4339b7f1cb5
cqc-melf <70640934+cqc-melf@users.noreply.github.com>
2026-01-27 12:51:41 +0000

    Update hugr-py/src/hugr/package.py
    Co-authored-by: Travis Thompson <102229498+trvto@users.noreply.github.com>

--- hugr-py/src/hugr/package.py
+++ hugr-py/src/hugr/package.py
@@ -125,7 +125,7 @@ class Package:
             assert isinstance(qir_str, str)
             return qir_str  # noqa: TRY300
         except ImportError as err:
-            raise ValueError(
+            raise ImportError(
                 "The `to_qir_bytes` method requires package hugr-qir to be installed."  # noqa: EM101
                 "For example via `pip install hugr-qir`"
             ) from err
