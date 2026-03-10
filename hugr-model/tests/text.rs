eb681408d01eef836b706fbf502e07495f03ee25
Craig Roy <craig.roy@quantinuum.com>
2026-03-10 16:12:48 +0000

    Use rstest's file mechanism

--- hugr-model/tests/text.rs
+++ hugr-model/tests/text.rs
@@ -10,22 +10,12 @@ fn roundtrip(source: &str) -> String {
 }
 
 #[rstest]
-#[case::add(include_str!("fixtures/model-add.edn"))]
-#[case::alias(include_str!("fixtures/model-alias.edn"))]
-#[case::call(include_str!("fixtures/model-call.edn"))]
-#[case::cfg(include_str!("fixtures/model-cfg.edn"))]
-#[case::cond(include_str!("fixtures/model-cond.edn"))]
-#[case::const_(include_str!("fixtures/model-const.edn"))]
-#[case::constraints(include_str!("fixtures/model-constraints.edn"))]
-#[case::decl_exts(include_str!("fixtures/model-decl-exts.edn"))]
-#[case::entrypoint(include_str!("fixtures/model-entrypoint.edn"))]
-#[case::lists(include_str!("fixtures/model-lists.edn"))]
-#[case::literals(include_str!("fixtures/model-literals.edn"))]
-#[case::loop_(include_str!("fixtures/model-loop.edn"))]
-#[case::order(include_str!("fixtures/model-order.edn"))]
-#[case::params(include_str!("fixtures/model-params.edn"))]
 #[cfg_attr(miri, ignore)] // Opening files is not supported in (isolated) miri
-pub fn test_roundtrip(#[case] expected: &str) {
+pub fn test_roundtrip(
+    #[files("tests/fixtures/*.edn")]
+    #[mode = str]
+    expected: &str,
+) {
     let actual = roundtrip(expected);
     // Trim whitespace from the strings to compare them
     let expected_trim = expected
