Quantinuum/hugr

commit 1d6af9a331b1dc68eddd3a0d5283e7b9dab6aa1c

feat: make `_composable_pass` and `_scope` public modules  (#2962)
closes #2961

hugr-py/src/hugr/passes/composable_pass.py
@@ -13,7 +13,7 @@ if TYPE_CHECKING:
     from collections.abc import Callable
 
     from hugr.hugr.base import Hugr
-    from hugr.passes._scope import PassScope
+    from hugr.passes.scope import PassScope
 
 
 # Type alias for a pass name
@@ -38,7 +38,7 @@ class ComposablePass(Protocol):
     def with_scope(self, scope: PassScope) -> ComposablePass:
         """Set the scope configuration for the pass.
 
-        As of `hugr 0.14.*`, this configuration is only a guidance, and may be
+        As of `hugr 0.14.*`, this configuration is only guidance, and may be
         ignored by the pass.
 
         In the future, passes will be required to respect the scope configuration.
@@ -142,7 +142,7 @@ class ComposedPass(ComposablePass):
     @property
     def name(self) -> PassName:
         names = [composable_pass.name for composable_pass in self.passes]
-        return f"Composed({ ', '.join(names) })"
+        return f"Composed({', '.join(names)})"
 
 
 @dataclass

hugr-py/tests/test_passes.py
@@ -6,7 +6,7 @@ from typing import TYPE_CHECKING
 import pytest
 
 from hugr.hugr.base import Hugr
-from hugr.passes._composable_pass import (
+from hugr.passes.composable_pass import (
     ComposablePass,
     ComposedPass,
     PassResult,
@@ -14,7 +14,7 @@ from hugr.passes._composable_pass import (
 )
 
 if TYPE_CHECKING:
-    from hugr.passes._scope import PassScope
+    from hugr.passes.scope import PassScope
 
 
 def test_composable_pass() -> None:

hugr-py/tests/test_scope.py
@@ -6,7 +6,7 @@ import pytest
 from hugr import tys, val
 from hugr.build import Module
 from hugr.hugr import Hugr, Node
-from hugr.passes._scope import GlobalScope, InScope, LocalScope, PassScope
+from hugr.passes.scope import GlobalScope, InScope, LocalScope, PassScope
 
 
 @dataclass(frozen=True)

commit 0a5664d2374d1bd18aedb4e7619d26d2f564b412

fix: allow both _composable_pass and composable_pass imports (#2965)
resolves breakage in #2962 by allowing both `from _composable_pass` and
`from composable_pass` imports. This is a problem as in tket-py we
access the private module `_composable_pass` for the implementation of
`PytketHugrPass` (not the best practice on my part).

I've added a duplicate` _composable_pass.py` file now with a wildcard
import (gross)

Will remove this once we have `_composable_pass` usage removed from
tket-py. There shouldn't be any `_scope` usage in the tket-py main
branch.

Theres probably a nicer way to do this with something like

```python
import sys
sys.modules["_foo"] = sys.modules[__name__]
```
However this wasn't immediately working for me.

Related #2961

---------

Co-authored-by: Maximilian Rüsch <maximilian.ruesch@quantinuum.com>

hugr-py/src/hugr/passes/_composable_pass.py
@@ -1,0 +1,20 @@
+"""A Protocol for a composable pass.
+
+Deprecated module alias for `hugr.passes.composable_pass`.
+"""
+
+from .composable_pass import (
+    ComposablePass,
+    ComposedPass,
+    PassName,
+    PassResult,
+    implement_pass_run,
+)
+
+__all__ = [
+    "PassName",
+    "PassResult",
+    "ComposedPass",
+    "ComposablePass",
+    "implement_pass_run",
+]

hugr-py/src/hugr/passes/composable_pass.py
@@ -1,7 +1,4 @@
-"""A Protocol for a composable pass.
-
-Currently unstable.
-"""
+"""A Protocol for a composable pass."""
 
 from __future__ import annotations
 

hugr-py/tests/test_passes.py
@@ -6,9 +6,14 @@ from typing import TYPE_CHECKING
 import pytest
 
 from hugr.hugr.base import Hugr
-from hugr.passes.composable_pass import (
+
+# Import some items from the deprecated module and some from the stable
+# version to ensure the deprecated module is still functional.
+from hugr.passes._composable_pass import (
     ComposablePass,
     ComposedPass,
+)
+from hugr.passes.composable_pass import (
     PassResult,
     implement_pass_run,
 )