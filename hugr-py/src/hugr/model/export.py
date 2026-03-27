Quantinuum/hugr

commit 8750a6b29ad93aaa72ff3fc48804e4f03b878d6c

fix: Use valid identifiers when constructing AST model (#2973)
Towards fixing #2970 .

Identifiers [must not begin with a
digit](https://github.com/Quantinuum/hugr/blob/f8f4b15fee09828a01c845775e605bbc6445bcd3/hugr-model/src/v0/ast/hugr.pest#L3).

hugr-py/src/hugr/model/export.py
@@ -574,7 +574,7 @@ class ModelExport:
         params = []
 
         for i, param_type in enumerate(param_types):
-            param_name = str(i)
+            param_name = f"_{i}"
 
             params.append(model.Param(name=param_name, type=param_type.to_model()))
 

hugr-py/src/hugr/tys.py
@@ -758,7 +758,7 @@ class Variable(Type):
         return f"${self.idx}"
 
     def to_model(self) -> model.Term:
-        return model.Var(str(self.idx))
+        return model.Var(f"_{self.idx}")
 
 
 @dataclass(frozen=True)

hugr-core/src/export.rs
@@ -828,7 +828,7 @@ impl<'a> Context<'a> {
             .expect("exporting poly func type outside of local scope");
         let visibility = self.bump.alloc(visibility);
         for (i, param) in t.params().iter().enumerate() {
-            let name = self.bump.alloc_str(&i.to_string());
+            let name = self.bump.alloc_str(&format!("_{}", i));
             let r#type = self.export_term(param, Some((scope, i as _)));
             let param = table::Param { name, r#type };
             params.push(param);

hugr-core/tests/snapshots/model__roundtrip_add.snap
@@ -16,10 +16,10 @@ expression: ast
 
 (declare-operation
   arithmetic.int.iadd
-  (param ?0 core.nat)
+  (param ?_0 core.nat)
   (core.fn
-    [(arithmetic.int.types.int ?0) (arithmetic.int.types.int ?0)]
-    [(arithmetic.int.types.int ?0)])
+    [(arithmetic.int.types.int ?_0) (arithmetic.int.types.int ?_0)]
+    [(arithmetic.int.types.int ?_0)])
   (meta
     (core.meta.description
       "addition modulo 2^N (signed and unsigned versions are the same op)")))

hugr-core/tests/snapshots/model__roundtrip_cfg.snap
@@ -16,36 +16,40 @@ expression: ast
 
 (import core.fn)
 
-(define-func public example.cfg_loop (param ?0 core.type) (core.fn [?0] [?0])
+(define-func public example.cfg_loop (param ?_0 core.type) (core.fn [?_0] [?_0])
   (dfg [%0] [%1]
-    (signature (core.fn [?0] [?0]))
+    (signature (core.fn [?_0] [?_0]))
     (cfg [%0] [%1]
-      (signature (core.fn [?0] [?0]))
+      (signature (core.fn [?_0] [?_0]))
       (cfg [%2] [%3]
-        (signature (core.ctrl [[?0]] [[?0]]))
+        (signature (core.ctrl [[?_0]] [[?_0]]))
         (block [%2] [%3 %2]
-          (signature (core.ctrl [[?0]] [[?0] [?0]]))
+          (signature (core.ctrl [[?_0]] [[?_0] [?_0]]))
           (dfg [%4] [%5]
-            (signature (core.fn [?0] [(core.adt [[?0] [?0]])]))
+            (signature (core.fn [?_0] [(core.adt [[?_0] [?_0]])]))
             ((core.make_adt 0) [%4] [%5]
-              (signature (core.fn [?0] [(core.adt [[?0] [?0]])])))))))))
+              (signature (core.fn [?_0] [(core.adt [[?_0] [?_0]])])))))))))
 
-(define-func public example.cfg_order (param ?0 core.type) (core.fn [?0] [?0])
+(define-func
+  public
+  example.cfg_order
+  (param ?_0 core.type)
+  (core.fn [?_0] [?_0])
   (dfg [%0] [%1]
-    (signature (core.fn [?0] [?0]))
+    (signature (core.fn [?_0] [?_0]))
     (cfg [%0] [%1]
-      (signature (core.fn [?0] [?0]))
+      (signature (core.fn [?_0] [?_0]))
       (cfg [%2] [%3]
-        (signature (core.ctrl [[?0]] [[?0]]))
+        (signature (core.ctrl [[?_0]] [[?_0]]))
         (block [%2] [%6]
-          (signature (core.ctrl [[?0]] [[?0]]))
+          (signature (core.ctrl [[?_0]] [[?_0]]))
           (dfg [%4] [%5]
-            (signature (core.fn [?0] [(core.adt [[?0]])]))
+            (signature (core.fn [?_0] [(core.adt [[?_0]])]))
             ((core.make_adt 0) [%4] [%5]
-              (signature (core.fn [?0] [(core.adt [[?0]])])))))
+              (signature (core.fn [?_0] [(core.adt [[?_0]])])))))
         (block [%6] [%3]
-          (signature (core.ctrl [[?0]] [[?0]]))
+          (signature (core.ctrl [[?_0]] [[?_0]]))
           (dfg [%7] [%8]
-            (signature (core.fn [?0] [(core.adt [[?0]])]))
+            (signature (core.fn [?_0] [(core.adt [[?_0]])]))
             ((core.make_adt 0) [%7] [%8]
-              (signature (core.fn [?0] [(core.adt [[?0]])])))))))))
+              (signature (core.fn [?_0] [(core.adt [[?_0]])])))))))))

hugr-core/tests/snapshots/model__roundtrip_cond.snap
@@ -18,8 +18,8 @@ expression: ast
 
 (declare-operation
   arithmetic.int.ineg
-  (param ?0 core.nat)
-  (core.fn [(arithmetic.int.types.int ?0)] [(arithmetic.int.types.int ?0)])
+  (param ?_0 core.nat)
+  (core.fn [(arithmetic.int.types.int ?_0)] [(arithmetic.int.types.int ?_0)])
   (meta
     (core.meta.description
       "negation modulo 2^N (signed and unsigned versions are the same op)")))

hugr-core/tests/snapshots/model__roundtrip_const.snap
@@ -101,5 +101,5 @@ expression: ast
 (declare-func
   public
   example.const_as_param
-  (param ?0 (core.const arithmetic.float.types.float64))
+  (param ?_0 (core.const arithmetic.float.types.float64))
   (core.fn [] [arithmetic.float.types.float64]))

hugr-core/tests/snapshots/model__roundtrip_constraints.snap
@@ -21,26 +21,26 @@ expression: ast
 (declare-func
   private
   _1
-  (param ?0 core.nat)
-  (param ?1 core.type)
-  (where (core.nonlinear ?1))
-  (core.fn [?1] [(collections.array.array ?0 ?1)])
+  (param ?_0 core.nat)
+  (param ?_1 core.type)
+  (where (core.nonlinear ?_1))
+  (core.fn [?_1] [(collections.array.array ?_0 ?_1)])
   (meta (core.title "array.replicate")))
 
 (declare-func
   public
   array.copy
-  (param ?0 core.nat)
-  (param ?1 core.type)
-  (where (core.nonlinear ?1))
+  (param ?_0 core.nat)
+  (param ?_1 core.type)
+  (where (core.nonlinear ?_1))
   (core.fn
-    [(collections.array.array ?0 ?1)]
-    [(collections.array.array ?0 ?1) (collections.array.array ?0 ?1)]))
+    [(collections.array.array ?_0 ?_1)]
+    [(collections.array.array ?_0 ?_1) (collections.array.array ?_0 ?_1)]))
 
 (define-func
   public
   util.copy
-  (param ?0 core.type)
-  (where (core.nonlinear ?0))
-  (core.fn [?0] [?0 ?0])
-  (dfg [%0] [%0 %0] (signature (core.fn [?0] [?0 ?0]))))
+  (param ?_0 core.type)
+  (where (core.nonlinear ?_0))
+  (core.fn [?_0] [?_0 ?_0])
+  (dfg [%0] [%0 %0] (signature (core.fn [?_0] [?_0 ?_0]))))

commit 8aab6366134db1ffdb7e65d70c9bcd04d661851d

fix: Prefix names with underscores during serialization (#2989)
Fixes #2988 .

This makes all the guppylang tests pass again. (Many were broken by
#2973 .)

It would be good to add a test case local to hugr; I will work on that,
but propose merging this as is since it fixes a critical issue.

hugr-py/src/hugr/model/export.py
@@ -59,10 +59,10 @@ class ModelExport:
         if root in self.link_names:
             return self.link_names[root]
         else:
-            index = str(self.link_next)
+            name = f"_{self.link_next}"
             self.link_next += 1
-            self.link_names[root] = index
-            return index
+            self.link_names[root] = name
+            return name
 
     def export_node(
         self, node: Node, virtual_input_links: Sequence[str] = []
@@ -526,7 +526,7 @@ class ModelExport:
                         source_types = model.List(
                             [type.to_model() for type in op.inputs]
                         )
-                        source = str(self.link_next)
+                        source = f"_{self.link_next}"
                         self.link_next += 1
 
                         child_node = self.export_node(

hugr-py/src/hugr/tys.py
@@ -596,7 +596,7 @@ class VariableArg(TypeArg):
         return f"${self.idx}"
 
     def to_model(self) -> model.Term:
-        return model.Var(str(self.idx))
+        return model.Var(f"_{self.idx}")
 
     def _resolve_used_extensions(
         self, registry: ExtensionRegistry | None = None
@@ -778,7 +778,7 @@ class RowVariable(Type):
         return f"${self.idx}"
 
     def to_model(self):
-        return model.Splice(model.Var(str(self.idx)))
+        return model.Splice(model.Var(f"_{self.idx}"))
 
 
 @dataclass(frozen=True)