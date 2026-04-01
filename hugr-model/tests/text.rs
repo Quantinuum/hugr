9c47e01fcac19beeb5412a9125335600d085c4d5
Craig Roy <craig.roy@quantinuum.com>
2026-04-01 11:06:26 +0100

    Do roundtrip testing of hugr-model examples

--- hugr-model/tests/text.rs
+++ hugr-model/tests/text.rs
@@ -1,8 +1,16 @@
 #![allow(missing_docs)]
 
+use hugr_core::{Hugr, HugrView};
 use hugr_model::v0::ast;
 use rstest::rstest;
 use std::str::FromStr as _;
+
+fn envelope_parse(source: &mut String) -> Result<(), anyhow::Error> {
+    source.insert_str(0, "HUGRiHJv(@");
+    let hugr = Hugr::load_str(source, None)?;
+    hugr.validate()?;
+    Ok(())
+}
 
 fn roundtrip(source: &str) -> String {
     let package = ast::Package::from_str(source).unwrap();
@@ -15,7 +23,11 @@ pub fn test_roundtrip(
     #[files("tests/fixtures/*.edn")]
     #[mode = str]
     expected: &str,
-) {
+) -> Result<(), anyhow::Error> {
+    {
+        let mut s: String = expected.to_string();
+        envelope_parse(&mut s)?;
+    }
     let actual = roundtrip(expected);
     // Trim whitespace from the strings to compare them
     let expected_trim = expected
@@ -25,4 +37,5 @@ pub fn test_roundtrip(
         .split_whitespace()
         .fold(String::new(), |acc, s| acc + s);
     assert_eq!(expected_trim, actual_trim);
+    Ok(())
 }
