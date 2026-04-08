fbc676eb35f238750a7a4779cb735c67367174f5
Alan Lawrence <alan.lawrence@cambridgequantum.com>
2026-04-08 13:57:37 +0100

    Reimplement petgraph traits conditioning on T: LinkView, except Visitable

--- hugr-core/src/hugr/views/syn_edge.rs
+++ hugr-core/src/hugr/views/syn_edge.rs
@@ -1,7 +1,6 @@
 //! Implementations of petgraph's traits for Hugr Region views.
 use petgraph::visit as pv;
-use portgraph::NodeIndex as NIdx;
-use pv::EdgeRef as _;
+use portgraph::{LinkView, NodeIndex as NIdx, PortOffset};
 
 /// Wraps some property of an edge that may either be an original edge in the Hugr
 /// or a synthetic edge - currently only those added for ordering constraints,
@@ -17,91 +16,90 @@ pub enum MaybeSynEdge<T> {
     NonLocalOrderingConstraint,
 }
 
-/// Wrapper for a petgraph that adds some extra edges.
+/// Wrapper for a [LinkView] that implements petgraph traits and adds some extra edges.
 #[derive(Clone, Debug)]
-pub(super) struct SynEdgeWrapper<T> {
+pub(super) struct SynEdgeWrapper<T: LinkView> {
     pub(super) region_view: T,
-    pub(super) syn_edges: Vec<(NIdx, NIdx)>,
+    #[allow(clippy::type_complexity)]
+    pub(super) syn_edges: Vec<(NIdx<T::NodeIndexBase>, NIdx<T::NodeIndexBase>)>,
 }
 
-impl<T: pv::GraphBase> pv::GraphBase for SynEdgeWrapper<T> {
-    type NodeId = T::NodeId;
-    type EdgeId = MaybeSynEdge<T::EdgeId>;
+impl<T: LinkView> pv::GraphBase for SynEdgeWrapper<T> {
+    type NodeId = NIdx<T::NodeIndexBase>;
+    type EdgeId = (
+        NIdx<T::NodeIndexBase>,
+        NIdx<T::NodeIndexBase>,
+        MaybeSynEdge<(PortOffset<T::PortOffsetBase>, PortOffset<T::PortOffsetBase>)>,
+    );
 }
 
-impl<T: pv::GraphBase> pv::GraphProp for SynEdgeWrapper<T> {
+impl<T: LinkView> pv::GraphProp for SynEdgeWrapper<T> {
     type EdgeType = petgraph::Directed;
 }
 
-impl<T: pv::GraphBase + pv::NodeCount> pv::NodeCount for SynEdgeWrapper<T> {
+impl<T: LinkView> pv::NodeCount for SynEdgeWrapper<T> {
     fn node_count(&self) -> usize {
         self.region_view.node_count()
     }
 }
 
-impl<T: pv::GraphBase + pv::NodeIndexable> pv::NodeIndexable for SynEdgeWrapper<T> {
+impl<T: LinkView> pv::NodeIndexable for SynEdgeWrapper<T> {
     fn node_bound(&self) -> usize {
-        self.region_view.node_bound()
+        // ALAN copied from petgraph; but are `NodeIndex`es always dense?
+        self.region_view.node_count()
     }
 
     fn to_index(&self, ix: Self::NodeId) -> usize {
-        self.region_view.to_index(ix)
+        ix.index()
     }
 
     fn from_index(&self, ix: usize) -> Self::NodeId {
-        self.region_view.from_index(ix)
+        NIdx::new(ix)
     }
 }
 
-impl<T: pv::GraphBase + pv::EdgeCount> pv::EdgeCount for SynEdgeWrapper<T> {
+impl<T: LinkView> pv::EdgeCount for SynEdgeWrapper<T> {
     fn edge_count(&self) -> usize {
-        self.region_view.edge_count() + self.syn_edges.len()
+        self.region_view.link_count() + self.syn_edges.len()
     }
 }
 
-impl<T: pv::GraphBase + pv::Data> pv::Data for SynEdgeWrapper<T> {
+impl<T: LinkView> pv::Data for SynEdgeWrapper<T> {
     /// Turns out the underlying [FlatRegion] has unit node weights, we may want to fix that.
     ///
     /// [FlatRegion]: portgraph::view::FlatRegion
-    type NodeWeight = T::NodeWeight;
+    type NodeWeight = ();
 
-    /// Turns out the underlying [FlatRegion] has unit node weights; we may want to fix that,
+    /// Turns out the underlying [FlatRegion] has unit edge weights; we may want to fix that,
     /// but at least this distinguishes synthetic edges from original edges.
     ///
     /// [FlatRegion]: portgraph::view::FlatRegion
-    type EdgeWeight = MaybeSynEdge<T::EdgeWeight>;
+    type EdgeWeight = MaybeSynEdge<(PortOffset<T::PortOffsetBase>, PortOffset<T::PortOffsetBase>)>;
 }
 
-impl<T: pv::GraphBase + pv::IntoNodeReferences> pv::IntoNodeReferences for &SynEdgeWrapper<T> {
-    type NodeRef = T::NodeRef;
-    type NodeReferences = T::NodeReferences;
+impl<'a, T: LinkView> pv::IntoNodeReferences for &'a SynEdgeWrapper<T> {
+    type NodeRef = NIdx<T::NodeIndexBase>;
+    type NodeReferences = Box<dyn Iterator<Item = NIdx<T::NodeIndexBase>> + 'a>;
 
     fn node_references(self) -> Self::NodeReferences {
-        self.region_view.node_references()
+        Box::new(self.region_view.nodes_iter())
     }
 }
 
-impl<'a, T: pv::GraphBase> pv::IntoNodeIdentifiers for &'a SynEdgeWrapper<T>
-where
-    &'a T: pv::IntoNodeIdentifiers<NodeId = T::NodeId>,
-{
-    type NodeIdentifiers = <&'a T as pv::IntoNodeIdentifiers>::NodeIdentifiers;
+impl<'a, T: LinkView> pv::IntoNodeIdentifiers for &'a SynEdgeWrapper<T> {
+    type NodeIdentifiers = Box<dyn Iterator<Item = Self::NodeId> + 'a>;
 
     fn node_identifiers(self) -> Self::NodeIdentifiers {
-        self.region_view.node_identifiers()
+        pv::IntoNodeReferences::node_references(self)
     }
 }
 
-impl<'a, T> pv::IntoNeighbors for &'a SynEdgeWrapper<T>
-where
-    T: pv::GraphBase<NodeId = NIdx>,
-    &'a T: pv::IntoNeighbors<NodeId = NIdx, Neighbors: 'a>,
-{
-    type Neighbors = Box<dyn Iterator<Item = NIdx> + 'a>;
+impl<'a, T: LinkView> pv::IntoNeighbors for &'a SynEdgeWrapper<T> {
+    type Neighbors = Box<dyn Iterator<Item = NIdx<T::NodeIndexBase>> + 'a>;
 
     fn neighbors(self, n: Self::NodeId) -> Self::Neighbors {
         Box::new(
-            self.region_view.neighbors(n).chain(
+            self.region_view.output_neighbours(n).chain(
                 self.syn_edges
                     .iter()
                     .filter_map(move |(src, dst)| (*src == n).then_some(*dst)),
@@ -112,19 +110,16 @@ where
 
 /// An edge in the derived graph - either an original edge, or a synthetic edge.
 #[derive(Debug, Copy, Clone, PartialEq, Eq)]
-pub struct EdgeRef<W, ID> {
-    src: NIdx,
-    tgt: NIdx,
-    //Either both will be original, or both synthetic,
-    // but since we have to return a reference from `fn weight()` we cannot store together.`
-    weight: MaybeSynEdge<W>,
-    id: MaybeSynEdge<ID>,
+pub struct EdgeRef<NId, PId> {
+    src: NId,
+    tgt: NId,
+    weight: MaybeSynEdge<(PId, PId)>,
 }
 
-impl<W: Copy, ID: Copy> pv::EdgeRef for EdgeRef<W, ID> {
-    type NodeId = NIdx;
-    type EdgeId = MaybeSynEdge<ID>;
-    type Weight = MaybeSynEdge<W>;
+impl<NId: Copy, PId: Copy> pv::EdgeRef for EdgeRef<NId, PId> {
+    type NodeId = NId;
+    type EdgeId = (NId, NId, MaybeSynEdge<(PId, PId)>);
+    type Weight = MaybeSynEdge<(PId, PId)>;
 
     fn source(&self) -> Self::NodeId {
         self.src
@@ -139,46 +134,44 @@ impl<W: Copy, ID: Copy> pv::EdgeRef for EdgeRef<W, ID> {
     }
 
     fn id(&self) -> Self::EdgeId {
-        self.id
+        (self.src, self.tgt, self.weight)
     }
 }
 
-impl<'a, T, W: Copy> pv::IntoEdgeReferences for &'a SynEdgeWrapper<T>
-where
-    T: pv::GraphBase<NodeId = NIdx> + pv::Data<EdgeWeight = W>,
-    &'a T: pv::IntoEdgeReferences<
-            EdgeRef: pv::EdgeRef<NodeId = NIdx, Weight = W, EdgeId = T::EdgeId> + 'a,
-        >,
-{
-    type EdgeRef = EdgeRef<W, T::EdgeId>;
+impl<'a, T: LinkView> pv::IntoEdgeReferences for &'a SynEdgeWrapper<T> {
+    type EdgeRef = EdgeRef<NIdx<T::NodeIndexBase>, PortOffset<T::PortOffsetBase>>;
     type EdgeReferences = Box<dyn Iterator<Item = Self::EdgeRef> + 'a>;
 
     fn edge_references(self) -> Self::EdgeReferences {
         Box::new(
             self.region_view
-                .edge_references()
-                .map(|er| EdgeRef {
-                    src: er.source(),
-                    tgt: er.target(),
-                    id: MaybeSynEdge::Original(er.id()),
-                    weight: MaybeSynEdge::Original(*er.weight()),
+                .nodes_iter()
+                .flat_map(|n| {
+                    self.region_view
+                        .links(n, portgraph::Direction::Outgoing)
+                        .map(|(src, tgt)| {
+                            let er: Self::EdgeRef = EdgeRef {
+                                src: self.region_view.port_node(src).unwrap(),
+                                tgt: self.region_view.port_node(tgt).unwrap(),
+                                weight: MaybeSynEdge::Original((
+                                    self.region_view.port_offset(src).unwrap(),
+                                    self.region_view.port_offset(tgt).unwrap(),
+                                )),
+                            };
+                            er
+                        })
                 })
                 .chain(self.syn_edges.iter().map(|(src, tgt)| EdgeRef {
                     src: *src,
                     tgt: *tgt,
-                    id: MaybeSynEdge::NonLocalOrderingConstraint,
                     weight: MaybeSynEdge::NonLocalOrderingConstraint,
                 })),
         )
     }
 }
 
-impl<'a, T> pv::IntoNeighborsDirected for &'a SynEdgeWrapper<T>
-where
-    T: pv::GraphBase<NodeId = NIdx>,
-    &'a T: pv::IntoNeighborsDirected<NodeId = NIdx, NeighborsDirected: 'a>,
-{
-    type NeighborsDirected = Box<dyn Iterator<Item = NIdx> + 'a>;
+impl<'a, T: LinkView> pv::IntoNeighborsDirected for &'a SynEdgeWrapper<T> {
+    type NeighborsDirected = Box<dyn Iterator<Item = NIdx<T::NodeIndexBase>> + 'a>;
 
     fn neighbors_directed(
         self,
@@ -187,7 +180,7 @@ where
     ) -> Self::NeighborsDirected {
         Box::new(
             self.region_view
-                .neighbors_directed(n, d)
+                .neighbours(n, d.into())
                 .chain(self.syn_edges.iter().filter_map(move |(src, dst)| match d {
                     petgraph::Direction::Outgoing if *src == n => Some(*dst),
                     petgraph::Direction::Incoming if *dst == n => Some(*src),
@@ -197,7 +190,9 @@ where
     }
 }
 
-impl<T: pv::GraphBase + pv::Visitable> pv::Visitable for SynEdgeWrapper<T> {
+impl<T: LinkView + pv::Visitable<Map: pv::VisitMap<NIdx<T::NodeIndexBase>>>> pv::Visitable
+    for SynEdgeWrapper<T>
+{
     type Map = T::Map;
 
     fn visit_map(&self) -> Self::Map {
